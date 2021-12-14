359b855ae36eec3f95a0ec6ff9b7a1007bf82e7b
Gregory Gerard <foreignbyproxy@gmail.com>
2021-12-14 13:24:30 -0500

    Set up Chakra

---
+++ components/wrappers/Layout/Layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Box, Container } from "@chakra-ui/react";
+
+import Navigation from "../../navigation/Navigation/Navigation";
+
+interface Props {
+	children: React.ReactChild;
+	containerWidth: string;
+	containerHeight: string;
+}
+
+export default function Layout({
+	children,
+	containerWidth = "container.xl",
+	containerHeight = "100vh",
+}: Props) {
+	return (
+		<Box>
+			<Navigation />
+			<Container minH={containerHeight} maxW={containerWidth} my={4} py={4}>
+				{children}
+			</Container>
+		</Box>
+	);
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+
+import theme from "../constants/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider>
+			<Component {...pageProps} theme={theme}/>
+		</ChakraProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
